83d56798f73cf6299a72b65774d738c027c6094e
Nichole <nicholebaiel@gmail.com>
2022-06-01 16:15:19 -0500

    Netlify CMS setup.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets/');
+  eleventyConfig.addPassthroughCopy('./src/admin');
+
   eleventyConfig.addWatchTarget("./src/css/");
 
   // eleventyConfig.addCollection("navigation", function (collection) {
